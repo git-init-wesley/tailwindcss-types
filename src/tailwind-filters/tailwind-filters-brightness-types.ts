95cd9241fa112b08a01e11cedbf23062c3350f79
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-19 07:16:09 +0100

    Update Tailwind Filters
    - Contrast | Brightness
    - Add/Update documentation
    - Generate/Update TypeDoc

--- src/tailwind-filters/tailwind-filters-brightness-types.ts
+++ src/tailwind-filters/tailwind-filters-brightness-types.ts
@@ -1,3 +1,148 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(0);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_0.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_0 = 'brightness-0'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(.5);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_50.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_50 = 'brightness-50'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(.75);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_75.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_75 = 'brightness-75'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(.9);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_90.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_90 = 'brightness-90'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(.95);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_95.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_95 = 'brightness-95'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(1);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_100.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_100 = 'brightness-100'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(1.05);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_105.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_105 = 'brightness-105'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(1.1);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_110.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_110 = 'brightness-110'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(1.25);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_125.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_125 = 'brightness-125'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(1.5);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_150.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_150 = 'brightness-150'
+
+/**
+ * @group Filters
+ * @category Brightness
+ * @css
+ * ```css
+ * filter: brightness(2);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/brightness)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/brightness_200.html)
+ * @since Tailwind v3.2.7
+ */
+export type brightness_200 = 'brightness-200'
+
 /**
  * @group Filters
  * @category Brightness
@@ -11,14 +156,14 @@
  * @since Tailwind v3.2.7
  */
 export type Brightness =
-    'brightness-0'
-    | 'brightness-50'
-    | 'brightness-75'
-    | 'brightness-90'
-    | 'brightness-95'
-    | 'brightness-100'
-    | 'brightness-105'
-    | 'brightness-110'
-    | 'brightness-125'
-    | 'brightness-150'
-    | 'brightness-200'
+    brightness_0
+    | brightness_50
+    | brightness_75
+    | brightness_90
+    | brightness_95
+    | brightness_100
+    | brightness_105
+    | brightness_110
+    | brightness_125
+    | brightness_150
+    | brightness_200
