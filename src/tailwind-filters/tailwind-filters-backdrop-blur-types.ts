eaafb29b4349fc45da66f54baf4dbb239e5db6b8
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-19 08:47:09 +0100

    Update Tailwind Filters BackdropBlur
    - Add/Update documentation
    - Generate/Update TypeDoc

---
+++ src/tailwind-filters/tailwind-filters-backdrop-blur-types.ts
@@ -0,0 +1,127 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(0);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_none.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_none = 'backdrop-blur-none'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(4px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_sm.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_sm = 'backdrop-blur-sm'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(8px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur = 'backdrop-blur'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(12px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_md.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_md = 'backdrop-blur-md'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(16px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_lg.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_lg = 'backdrop-blur-lg'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(24px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_xl.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_xl = 'backdrop-blur-xl'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(40px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_2xl.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_2xl = 'backdrop-blur-2xl'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @css
+ * ```css
+ * backdrop-filter: blur(64px);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_blur_3xl.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_blur_3xl = 'backdrop-blur-3xl'
+
+/**
+ * @group Filters
+ * @category Backdrop Blur
+ * @example
+ * ```ts
+ * let value: BackdropBlur //Only tailwindcss types
+ * let value: string | BackdropBlur //Your custom tailwindcss with only tailwindcss type
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-blur)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/BackdropBlur.html)
+ * @since Tailwind v3.2.7
+ */
+export type BackdropBlur =
+    backdrop_blur_none
+    | backdrop_blur_sm
+    | backdrop_blur
+    | backdrop_blur_md
+    | backdrop_blur_lg
+    | backdrop_blur_xl
+    | backdrop_blur_2xl
+    | backdrop_blur_3xl
