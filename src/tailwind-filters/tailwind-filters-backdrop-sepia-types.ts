26f671d9fc4fc917ea15feec1a63fefa664b481e
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-21 17:16:07 +0100

    Update Tailwind Filters Backdrop Sepia
    - Add/Update documentation
    - Generate/Update TypeDoc

---
+++ src/tailwind-filters/tailwind-filters-backdrop-sepia-types.ts
@@ -0,0 +1,41 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Backdrop Sepia
+ * @css
+ * ```css
+ * backdrop-filter: sepia(0);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-sepia)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_sepia_0.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_sepia_0 = 'backdrop-sepia-0'
+
+/**
+ * @group Filters
+ * @category Backdrop Sepia
+ * @css
+ * ```css
+ * backdrop-filter: sepia(100%);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-sepia)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_sepia.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_sepia = 'backdrop-sepia'
+
+/**
+ * @group Filters
+ * @category Backdrop Sepia
+ * @example
+ * ```ts
+ * let value: BackdropSepia //Only tailwindcss types
+ * let value: string | BackdropSepia //Your custom tailwindcss with only tailwindcss type
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-sepia)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/BackdropSepia.html)
+ * @since Tailwind v3.2.7
+ */
+export type BackdropSepia = backdrop_sepia_0 | backdrop_sepia
