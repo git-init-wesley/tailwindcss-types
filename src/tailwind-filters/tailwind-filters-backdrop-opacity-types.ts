929c61d395bae12442412feb39f59adac9e4a113
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-21 17:15:44 +0100

    Update Tailwind Filters Backdrop Opacity
    - Add/Update documentation
    - Generate/Update TypeDoc

---
+++ src/tailwind-filters/tailwind-filters-backdrop-opacity-types.ts
@@ -0,0 +1,225 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_0.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_0 = 'backdrop-opacity-0'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(.05);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_5.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_5 = 'backdrop-opacity-5'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.1);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_10.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_10 = 'backdrop-opacity-10'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.2);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_20.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_20 = 'backdrop-opacity-20'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.25);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_25.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_25 = 'backdrop-opacity-25'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.3);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_30.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_30 = 'backdrop-opacity-30'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.4);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_40.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_40 = 'backdrop-opacity-40'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.5);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_50.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_50 = 'backdrop-opacity-50'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.6);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_60.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_60 = 'backdrop-opacity-60'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.7);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_70.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_70 = 'backdrop-opacity-70'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.75);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_75.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_75 = 'backdrop-opacity-75'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.8);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_80.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_80 = 'backdrop-opacity-80'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.9);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_90.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_90 = 'backdrop-opacity-90'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(0.95);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_95.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_95 = 'backdrop-opacity-95'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @css
+ * ```css
+ * backdrop-filter: opacity(1);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_opacity_100.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_opacity_100 = 'backdrop-opacity-100'
+
+/**
+ * @group Filters
+ * @category Backdrop Opacity
+ * @example
+ * ```ts
+ * let value: BackdropOpacity //Only tailwindcss types
+ * let value: string | BackdropOpacity //Your custom tailwindcss with only tailwindcss type
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-opacity)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/BackdropOpacity.html)
+ * @since Tailwind v3.2.7
+ */
+export type BackdropOpacity =
+    backdrop_opacity_0
+    | backdrop_opacity_5
+    | backdrop_opacity_10
+    | backdrop_opacity_20
+    | backdrop_opacity_25
+    | backdrop_opacity_30
+    | backdrop_opacity_40
+    | backdrop_opacity_50
+    | backdrop_opacity_60
+    | backdrop_opacity_70
+    | backdrop_opacity_75
+    | backdrop_opacity_80
+    | backdrop_opacity_90
+    | backdrop_opacity_95
+    | backdrop_opacity_100
