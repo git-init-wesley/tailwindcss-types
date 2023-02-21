eeb29c97b20a83e6621d4abd830c6861e40affe5
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-21 17:15:16 +0100

    Update Tailwind Filters Backdrop Hue Rotate
    - Add/Update documentation
    - Generate/Update TypeDoc

---
+++ src/tailwind-filters/tailwind-filters-backdrop-hue-rotate-types.ts
@@ -0,0 +1,99 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(0deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_0.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_0 = 'backdrop_hue-rotate-0'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(15deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_15.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_15 = 'backdrop_hue-rotate-15'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(30deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_30.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_30 = 'backdrop_hue-rotate-30'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(60deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_60.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_60 = 'backdrop_hue-rotate-60'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(90deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_90.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_90 = 'backdrop_hue-rotate-90'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @css
+ * ```css
+ * backdrop-filter: hue-rotate(180deg);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_hue_rotate_180.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_hue_rotate_180 = 'backdrop_hue-rotate-180'
+
+/**
+ * @group Filters
+ * @category Backdrop Hue Rotate
+ * @example
+ * ```ts
+ * let value: BackdropHueRotate //Only tailwindcss types
+ * let value: string | BackdropHueRotate //Your custom tailwindcss with only tailwindcss type
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-hue-rotate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/BackdropHueRotate.html)
+ * @since Tailwind v3.2.7
+ */
+export type BackdropHueRotate =
+    backdrop_hue_rotate_0
+    | backdrop_hue_rotate_15
+    | backdrop_hue_rotate_30
+    | backdrop_hue_rotate_60
+    | backdrop_hue_rotate_90
+    | backdrop_hue_rotate_180
