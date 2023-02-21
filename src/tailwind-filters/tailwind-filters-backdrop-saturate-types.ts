f49ed274343f7001b8db1357205ea64fd9bf43ad
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-02-21 17:15:56 +0100

    Update Tailwind Filters Backdrop Saturate
    - Add/Update documentation
    - Generate/Update TypeDoc

---
+++ src/tailwind-filters/tailwind-filters-backdrop-saturate-types.ts
@@ -0,0 +1,85 @@
+// noinspection JSUnusedGlobalSymbols
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @css
+ * ```css
+ * backdrop-filter: saturate(0);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_saturate_0.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_saturate_0 = 'backdrop-saturate-0'
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @css
+ * ```css
+ * backdrop-filter: saturate(.5);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_saturate_50.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_saturate_50 = 'backdrop-saturate-50'
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @css
+ * ```css
+ * backdrop-filter: saturate(1);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_saturate_100.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_saturate_100 = 'backdrop-saturate-100'
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @css
+ * ```css
+ * backdrop-filter: saturate(1.5);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_saturate_150.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_saturate_150 = 'backdrop-saturate-150'
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @css
+ * ```css
+ * backdrop-filter: saturate(2);
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/backdrop_saturate_200.html)
+ * @since Tailwind v3.2.7
+ */
+export type backdrop_saturate_200 = 'backdrop-saturate-200'
+
+/**
+ * @group Filters
+ * @category Backdrop Saturate
+ * @example
+ * ```ts
+ * let value: BackdropSaturate //Only tailwindcss types
+ * let value: string | BackdropSaturate //Your custom tailwindcss with only tailwindcss type
+ * ```
+ * @description You can refer to the official [tailwind documentation](https://tailwindcss.com/docs/backdrop-saturate)
+ * @description You can refer to the [typedoc](https://git-init-wesley.github.io/tailwindcss-types/docs/types/BackdropSaturate.html)
+ * @since Tailwind v3.2.7
+ */
+export type BackdropSaturate =
+    backdrop_saturate_0
+    | backdrop_saturate_50
+    | backdrop_saturate_100
+    | backdrop_saturate_150
+    | backdrop_saturate_200
